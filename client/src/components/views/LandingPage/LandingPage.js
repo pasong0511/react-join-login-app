b741da3a12cb07c2cd8eba3f9205e63a3beea488
pasong0511 <pasong0511@gmail.com>
2022-08-02 19:40:06 +0900

    로그아웃 구현 완료

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,25 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function LandingPage() {
+function LandingPage(props) {
+    let navigate = useNavigate();
+
     useEffect(() => {
         axios.get("/api/hello").then((response) => {
             console.log(response);
         });
     }, []);
+
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`).then((response) => {
+            if (response.data.success) {
+                navigate("/login");
+            } else {
+                alert("로그아웃 하는데 실패 했습니다.");
+            }
+        });
+    };
 
     return (
         <div
@@ -19,6 +32,8 @@ function LandingPage() {
             }}
         >
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>로그아웃</button>
         </div>
     );
 }
